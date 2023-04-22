37b8228f9c08523a88331cbbcaac5a72dd17299a
Silvia2811 <41351186+Silvia2811@users.noreply.github.com>
2023-04-21 23:27:19 -0300

    Ultima version

---
+++ Js/app2.js
@@ -0,0 +1,21 @@
+const $contenedor = document.getElementById('personajes');
+const URL_API = "https://rickandmortyapi.com/api/character";
+(fetch(URL_API))
+.then((response) => {
+    return response.json();
+})
+.then((data) =>{
+    const personajes = data.results;
+    
+    for(let i= 0; i< personajes.length ; i++){
+        const{name,gender,species,status,image} = personajes[i];
+
+        $contenedor.innerHTML += `<div class="grid-item">
+        <img src="${image}" alt="Imagen de ${name}
+        <h3>${name}</h3>
+        <p>Gender:${gender}</p>
+        <p>Species: ${species}</p>
+        <p>Status: ${status}</p>
+        </div>`
+    }
+});
